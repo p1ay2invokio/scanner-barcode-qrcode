6c45b5eaaa1de69f7172eeed8e46c3e11d778352
play21947 <play21947@gmail.com>
2024-11-01 17:39:18 +0700

    first

--- app/page.js
+++ app/page.js
@@ -1,101 +1,64 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import { useState } from 'react';
+import Html5QrcodePlugin from './scanner'
+import axios from 'axios';
+
+const App = (props) => {
+
+  let [modal, setModal] = useState(false)
+  let [found, setFound] = useState(false)
+
+  let [product, setProduct] = useState([])
+  let [qtyup, setQtyUp] = useState(null)
+
+  const onNewScanResult = (decodedText, decodedResult) => {
+    // handle decoded results here
+    console.log(decodedText)
+    console.log("Found")
+    setFound(true)
+    axios.get(`http://localhost:3001/api/product/${decodedText}`).then((res) => {
+      console.log(res.data)
+      setProduct(res.data)
+      setModal(true)
+      setQtyUp(res.data[0].qty)
+    })
+  };
+
+  const updateProduct = (barcode, qty) => {
+    axios.patch('http://localhost:3001/api/update_quantity', {
+      barcode: barcode,
+      qty: qty
+    }).then((res) => {
+      console.log(res.data)
+    })
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div>
+      <div>
+        <Html5QrcodePlugin
+          qrCodeSuccessCallback={onNewScanResult}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      {modal ? <div className='w-full h-full bg-black/80 fixed left-0 top-0 flex justify-center items-center'>
+        <div className='w-[300px] text-black h-[300px] bg-white flex justify-center items-center flex-col gap-[10px]'>
+          <div className='flex flex-col justify-center items-center'>
+            <p>{product[0].barcode}</p>
+            <p>{product[0].name}</p>
+          </div>
+          <input value={qtyup} onChange={(e) => {
+            setQtyUp(e.target.value)
+          }} className='border-[1px] border-black'></input>
+          <button onClick={() => {
+            setModal(false)
+            updateProduct(product[0].barcode, Number(qtyup))
+          }} className='w-[90%] h-[50px] bg-blue-400 text-white'>อัพเดท</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div> : null}
     </div>
   );
-}
+};
+
+export default App
